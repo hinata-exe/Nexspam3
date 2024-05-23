8aea2b5877e203c4557f4c4bcf02d202b6d60a74
ㅤ <121450897+hinata-exe@users.noreply.github.com>
2024-05-23 08:15:51 +0530

    Umm !

--- AltBots/modules/help.py
+++ AltBots/modules/help.py
@@ -34,7 +34,7 @@ async def help(event):
     if event.sender_id in SUDO_USERS:
         try:
           await event.client.send_file(event.chat_id,
-              "https://te.legra.ph/file/07d39b85c6cea32f15259.jpg",
+              "https://graph.org/file/881b423b947329ea9abfa.jpg",
               caption=HELP_STRING,
               buttons=HELP_BUTTON
               )
